5928b7b3be467dccab4ef2120bd5d269ca44fbd1
鑫鑫 <shou1wang@gmail.com>
2018-04-18 20:21:55 +0800

    blockBtn

---
+++ RuntimeAndRunloop/RuntimeAndRunloop/UIButton+ButtonBlockCategory.h
@@ -0,0 +1,13 @@
+//
+//  UIButton+ButtonBlockCategory.h
+//  RuntimeAndRunloop
+//
+//  Created by 鑫鑫 on 2018/4/18.
+//  Copyright © 2018年 xinxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (ButtonBlockCategory)
+
+@end
---
+++ RuntimeAndRunloop/RuntimeAndRunloop/UIButton+ButtonBlockCategory.m
@@ -0,0 +1,13 @@
+//
+//  UIButton+ButtonBlockCategory.m
+//  RuntimeAndRunloop
+//
+//  Created by 鑫鑫 on 2018/4/18.
+//  Copyright © 2018年 xinxin. All rights reserved.
+//
+
+#import "UIButton+ButtonBlockCategory.h"
+
+@implementation UIButton (ButtonBlockCategory)
+
+@end
