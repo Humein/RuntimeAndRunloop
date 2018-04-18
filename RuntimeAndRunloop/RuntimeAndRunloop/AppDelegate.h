2e990e3b3fc1cdcc97d6e7634d52e12f03877fbf
鑫鑫 <shou1wang@gmail.com>
2018-04-18 19:59:50 +0800

    first init

---
+++ RuntimeAndRunloop/RuntimeAndRunloop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimeAndRunloop
+//
+//  Created by 鑫鑫 on 2018/4/18.
+//  Copyright © 2018年 xinxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
